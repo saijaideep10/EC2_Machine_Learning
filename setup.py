b548208d98b12150e29a2ade8fbe5536a0054d94
saijaideep10 <saijaideep111@gmail.com>
2024-04-04 12:43:49 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "EC2_Machine_Learning"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "saijaideep10"
 SRC_REPO = "mlProject"
 AUTHOR_EMAIL = "saijaideep111@gmail.com"
 
@@ -15,14 +15,14 @@ AUTHOR_EMAIL = "saijaideep111@gmail.com"
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
-    author=saijaideep10,
-    author_email=saijaideep111@gmail.com,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
     description="A small python package for ml app",
     long_description=long_description,
     long_description_content="text/markdown",
-    url=f"https://github.com/saijaideep10/EC2_Machine_Learning",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker": f"https://github.com/saijaideep10/EC2_Machine_Learning/issues",
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
